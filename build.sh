10177896db720280ff8a199ea09d694c961a6e3c
lemogne <87395736+lemogne@users.noreply.github.com>
2025-01-14 23:47:11 +0100

    fix dumb mistake + add option for build.sh to compile desktop as library

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/sh
 rm -f -- libdesktop.so rwm
-separatelib=1
+separatelib=0
 args="-O3"
 for i in "$@"
 do
 	if [ "$i" = "DEBUG" ]; then
 		args="-O0 -g"
-	elif [ "$i" = "GENNOLIB" ]; then
-		separatelib=0
+	elif [ "$i" = "GENLIB" ]; then
+		separatelib=1
 	fi
 done
 
-if [ separatelib = 1 ]; then
+if [ $separatelib = 1 ]; then
 	g++ --std=c++17 -shared -o libdesktop.so -fPIC desktop.cpp 
 	g++ --std=c++17 $args rwm.cpp windows.cpp charencoding.cpp -o rwm -lncursesw -L. -ldesktop -lutil
 else 
