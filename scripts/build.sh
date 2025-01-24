b5215687c8035fc66596fdb0b741c23b62ecfe46
lemogne <87395736+lemogne@users.noreply.github.com>
2025-01-24 19:20:20 +0100

    sort files into directories

---
+++ scripts/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/sh
+rm -f -- libdesktop.so rwm
+separatelib=0
+args="-O3"
+for i in "$@"
+do
+	if [ "$i" = "DEBUG" ]; then
+		args="-O0 -g"
+	elif [ "$i" = "GENLIB" ]; then
+		separatelib=1
+	fi
+done
+
+cd ./source
+
+if [ $separatelib = 1 ]; then
+	g++ --std=c++17 -shared -o ../libdesktop.so -fPIC desktop.cpp 
+	g++ --std=c++17 $args rwm.cpp windows.cpp charencoding.cpp -o ../rwm -lncursesw -L. -ldesktop -lutil
+else 
+	g++ --std=c++17 $args rwm.cpp windows.cpp desktop.cpp charencoding.cpp -o ../rwm -lncursesw -lutil
+fi
+
+cd ..
